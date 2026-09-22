e3ceca6396f4ab51d4166299275dd485275c2743
Juan David Tafur Palomino <n00434097@upn.pe>
2026-09-21 20:16:48 -0500

    proyecto POO t1, encapsulamiento

---
+++ src/t1_poo/Alumno.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1_poo;
+
+/**
+ *
+ * @author UCA40424
+ */
+public class Alumno {
+    private String tip_doc;
+    private String num_doc;
+    private String nombre;
+    private String tipo_beca;
+    private String pors_beca;
+    private String cod_alumno;
+    private int Telefono;
+
+    public String getTip_doc() {
+        return tip_doc;
+    }
+
+    public void setTip_doc(String tip_doc) {
+        this.tip_doc = tip_doc;
+    }
+
+    public String getNum_doc() {
+        return num_doc;
+    }
+
+    public void setNum_doc(String num_doc) {
+        this.num_doc = num_doc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo_beca() {
+        return tipo_beca;
+    }
+
+    public void setTipo_beca(String tipo_beca) {
+        this.tipo_beca = tipo_beca;
+    }
+
+    public String getPors_beca() {
+        return pors_beca;
+    }
+
+    public void setPors_beca(String pors_beca) {
+        this.pors_beca = pors_beca;
+    }
+
+    public String getCod_alumno() {
+        return cod_alumno;
+    }
+
+    public void setCod_alumno(String cod_alumno) {
+        this.cod_alumno = cod_alumno;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+    
+}
---
+++ src/t1_poo/T1_POO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1_poo;
+
+/**
+ *
+ * @author UCA40424
+ */
+public class T1_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FIOJDS
+        
+    }
+    
+}
